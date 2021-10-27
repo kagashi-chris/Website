af0a81e56c097a98052049500af07771ed6abbce
Chris Zhen <chris.zhenartist@gmail.com>
2021-10-26 22:23:07 -0400

    added anchors in nav

--- src/Components/Layout/Header/HeaderNavElement.js
+++ src/Components/Layout/Header/HeaderNavElement.js
@@ -2,7 +2,13 @@ import React from "react";
 import classes from "./HeaderNavElement.module.css";
 
 const HeaderNavElement = (props) => {
-  return <li className={classes.nav__items}>{props.navName}</li>;
+  return (
+    <li className={classes.nav__items}>
+      <a class="main-nav-link" href="#">
+        {props.navName}
+      </a>
+    </li>
+  );
 };
 
 export default HeaderNavElement;
