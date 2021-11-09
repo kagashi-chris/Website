94e8653317c51aed7b2fb3fb9210d0e1fc205cb0
Chris Zhen <chris.zhenartist@gmail.com>
2021-11-08 22:43:11 -0500

    replaced panels with images

--- src/Components/Layout/Section1/Section1Panels.js
+++ src/Components/Layout/Section1/Section1Panels.js
@@ -2,14 +2,17 @@ import React from "react";
 import Section1PanelElement from "./Section1PanelElement";
 // import classes from "../../../general.css";
 import panelImg from "../../../assets/placeholderpanel.jpg";
+import frontEndImg from "../../../assets/frontend.jpg";
+import backEndImg from "../../../assets/backend.jpg";
+import artImg from "../../../assets/art.jpg";
 import classes from "./Section1Panels.module.css";
 
 const Section1Panel = () => {
   return (
     <div className={classes.panels}>
-      <Section1PanelElement panelImg={panelImg} />
-      <Section1PanelElement panelImg={panelImg} />
-      <Section1PanelElement panelImg={panelImg} />
+      <Section1PanelElement panelImg={frontEndImg} />
+      <Section1PanelElement panelImg={backEndImg} />
+      <Section1PanelElement panelImg={artImg} />
     </div>
   );
 };
