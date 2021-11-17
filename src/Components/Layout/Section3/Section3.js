c74b0ccb78e851a00cbbc621f3980274dabe2906
Chris Zhen <chris.zhenartist@gmail.com>
2021-11-17 11:27:10 -0500

    updated section3 panel element

--- src/Components/Layout/Section3/Section3.js
+++ src/Components/Layout/Section3/Section3.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Section3Panel from "./Section3Panel";
 import classes from "./Section3.module.css";
+import cssIcon from "../../../assets/CSS3Icon.png";
+import html5Icon from "../../../assets/HTML5Icon.png";
+import javaIcon from "../../../assets/JavaIcon.png";
 
 const Section3 = () => {
+  const project1Items = [
+    { jIcon: javaIcon },
+    { hIcon: html5Icon },
+    { cIcon: cssIcon },
+  ];
+
   return (
     <div className={classes.section3} id="projects">
       <h3 className={classes.textEffect}>
         <span>Projects</span>
       </h3>
-      <Section3Panel />
+      <Section3Panel iconList={project1Items} />
     </div>
   );
 };
--- src/Components/Layout/Section3/Section3PanelElement.js
+++ src/Components/Layout/Section3/Section3PanelElement.js
@@ -1,8 +1,12 @@
 import React from "react";
 import classes from "./Section3PanelElement.module.css";
 
-const Section3PanelElement = () => {
-  return <div className={classes.section3PanelElement}>&nbsp;</div>;
+const Section3PanelElement = (props) => {
+  return (
+    <div className={classes.section3PanelElement}>
+      for(var prop in obj) &nbsp;
+    </div>
+  );
 };
 
 export default Section3PanelElement;
